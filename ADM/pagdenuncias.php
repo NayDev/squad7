29f504045798cc47b06aa70a551a9d29cd08476b
matsuboshi <matsuboshi@hotmail.com>
2020-11-01 12:13:19 -0300

    ADM - pagina tabela denuncias

---
+++ ADM/pagdenuncias.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php require_once('connectdb.html') ?>
+  <?php require_once('bootstrap.html') ?>
+  <title>ADM SOS - Denúncias</title>
+</head>
+
+<body style="background-color: black;" class="text-white">
+
+  <?php include_once('menu.html') ?>
+
+  <div class="jumbotron card card-image text-white" style="background-color: black;">
+    <p class="text-center">Bem vindo ao sistema SOS, <b><?php echo $_SESSION['nome'] ?></b>! - (<?php echo $_SESSION['email'] ?>)</p>
+    <h1 class="display-4 text-danger  font-weight-bold">Denúncias</h1>
+    <hr class="my-4 bg-white">
+    <p class="lead font-weight-bold">Denúncias presentes no sistema:</p>
+
+    <table class="table table-striped table-hover table-dark bg-dark text-center">
+      <thead>
+        <tr>
+          <th scope="col" class=" text-left">ID-DEN</th>
+          <th scope="col" class=" text-left">UBS</th>
+          <th scope="col" class=" text-left">MEDICAMENTO</th>
+          <th scope="col" class=" text-left">DATA DENUNCIA</th>
+        </tr>
+      </thead>
+      <tbody>
+
+
+
+        <?php
+        $sql = "
+          SELECT
+          den.id id,
+          ubs.nome ubs,
+          med.nome med,
+          den.data_ocorrencia data
+          FROM
+          denuncia den
+          JOIN ubs ON den.ubs_id = ubs.id
+          JOIN medicamento med ON den.medicamento_id = med.id;
+        ";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+          while ($rows = $result->fetch_assoc()) {
+        ?>
+
+            <tr>
+              <th class="align-middle text-left" scope="row "><?php echo $rows["id"]; ?></th>
+              <td class="align-middle text-left"><?php echo $rows["ubs"]; ?></td>
+              <td class="align-middle text-left"><?php echo $rows["med"]; ?></td>
+              <td class="align-middle text-left"><?php echo $rows["data"]; ?></td>
+              <td class="align-middle text-right">
+                <div class="btn-group">
+                  <button class="btn btn-outline-danger font-weight-bold">VER</button>
+                  <button class="btn btn-outline-danger font-weight-bold">APAGAR</button>
+                </div>
+              </td>
+            </tr>
+
+        <?php
+          }
+        } else {
+          echo "Nenhum produto cadastrado!";
+        }
+        ?>
+
+      </tbody>
+    </table>
+
+
+  </div>
+
+  <section id="displayInfo"></section>
+
+
+  <footer class="mb-5">
+    <div class="btn-group pagination justify-content-center">
+      <a class="btn btn-outline-light disabled" href="#">ANTERIOR</a>
+      <a class="btn btn-outline-light active" href="#">1</a>
+      <a class="btn btn-outline-light" href="#">2</a>
+      <a class="btn btn-outline-light" href="#">3</a>
+      <a class="btn btn-outline-light" href="#">PRÓXIMO</a>
+    </div>
+  </footer>
+
+
+
+  <script>
+    function showInfo(id, nome) {
+      displayInfo.innerHTML += `
+      <h1 class="alert alert-info alert-dismissible">
+      <button class="close" data-dismiss="alert">&times;</button>
+      Sem info para este evento para a UBS ${id} -  ${nome} 
+      </h1>
+      `
+    }
+  </script>
+
+
+</body>
+
+</html>
